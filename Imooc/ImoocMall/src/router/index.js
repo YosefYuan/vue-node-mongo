5f61a0fa5369addd32172136e636b52dea9e4578
YosefYuan <yuanruixiangjet@163.com>
2017-09-05 23:48:05 +0800

    0905 night

--- Imooc/ImoocMall/src/router/index.js
+++ Imooc/ImoocMall/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import GoodsList from './../views/GoodsList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    mode: 'hash',
+    routes: [{
+        path: '/goods/:goodsId/user/:name',
+        name: 'GoodsList',
+        component: GoodsList
+    }]
 })
---
+++ Imooc/ImoocMall/src/views/GoodsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    这是商品列表页面  
+    <span>{{$route.params.goodsId}}</span>    
+    <span>{{$route.params.name}}</span>    
+  </div>
+</template>
+<style>
+    
+</style>
+<script>
+export default {
+  data(){
+      return {
+          msg:'hello vue'
+      }
+  }
+}
+</script>
+
+
